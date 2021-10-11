6206318361a3914738d4357580ad0ae940ecab82
KENNEDY <JOHNSAMUELKENNEDY1@GMAIL.COM>
2021-10-11 17:04:18 -0400

    BounceBall Game - JK

--- GameCode.swift
+++ GameCode.swift
@@ -16,23 +16,9 @@ let funnelPoints = [
     Point(x: 60, y: 0),
     Point(x: 20, y: 0)
 ]
-
-
 //CREATES THE SHAPE "FUNNEL" USING THE DIMENSIONS FROM THE funnelPoints ARRAY
 let funnel = PolygonShape(points:
    funnelPoints)
-
-
-//CREATES THE DIAMOND TARGET
-let targetPoints = [
-    Point(x: 10, y: 0),
-    Point(x: 0, y: 10),
-    Point(x: 10, y: 20),
-    Point(x: 20, y: 10)
-]
-
-let target = PolygonShape(points:
-   targetPoints)
 
 
 /*
@@ -55,7 +41,7 @@ fileprivate func setupBall() {
     ball.fillColor = .blue
     ball.onCollision = ballCollided(with:)
     ball.isDraggable = false
-    ball.bounciness = 0.6
+    ball.bounciness = 1.0
     scene.trackShape(ball)
     ball.onExitedScene = ballExitedScene
     ball.onTapped = resetGame
@@ -70,7 +56,6 @@ fileprivate func addBarrier(at position: Point, width: Double, height: Double, a
         Point(x: width, y: height),
         Point(x: width, y: 0)
     ]
-    
     let barrier = PolygonShape(points: barrierPoints)
     barriers.append(barrier)
         
@@ -84,8 +69,9 @@ fileprivate func addBarrier(at position: Point, width: Double, height: Double, a
     
     //LOCKS THE BARRIER IN ITS LOCATION
     barrier.isImmobile = true
+    
     //USED FOR GETTING THE COORDINATES OF THE SHAPES TO FEED INTO THE ARRAYS
-    scene.onShapeMoved = printPosition(of:)
+    //scene.onShapeMoved = printPosition(of:)
 }
 
 fileprivate func setupFunnel() {
@@ -97,12 +83,10 @@ fileprivate func setupFunnel() {
     funnel.isDraggable = false
 }
 
-
 //TEMPORARY FUNCTION CREATED TO HELP LOCATE THE TARGETS AND BARRIERS
-func printPosition(of shape: Shape) {
-    print(shape.position)
-}
-
+//func printPosition(of shape: Shape) {
+//    print(shape.position)
+//}
 
 // HANDLES COLLISIONS BETWEEN THE BALL AND THE TARGETS
 func ballCollided(with otherShape: Shape) {
@@ -121,30 +105,29 @@ func ballExitedScene() {
 //CREATES THE TARGETS
 func addTarget(at position: Point) {            //pg253
     let targetPoints = [
-        Point(x: 0, y: 0),
+        Point(x: 10, y: 0),
         Point(x: 0, y: 10),
         Point(x: 10, y: 20),
         Point(x: 20, y: 10)
-
          ]
     
     let target = PolygonShape(points: targetPoints)
     targets.append(target)
-    
-    
+   
+    target.name = "target"
     //ADDS TARGET TO THE SCENE
     target.position = position
     //CHARACTERISTICS OF TARGET
     target.hasPhysics = true
     target.isImmobile = true
     target.isImpermeable = true
-    target.fillColor = .orange
+    target.fillColor = .red
     target.isDraggable = false
     scene.add(target)
-    
-    //TEMPORARY CODE TO HELP SET LOCATIONS
-    scene.onShapeMoved = printPosition(of:)
 }
+    //TEMPORARY CODE TO HELP SET LOCATIONS
+//    scene.onShapeMoved = printPosition(of:)
+//}
 
 
 //DROPS THE BALL BY MOVING THE BALL TO THE FUNNEL'S POSITION
@@ -154,11 +137,15 @@ func dropBall() {
     for barrier in barriers {
         barrier.isDraggable = true
     }
+    for target in targets {
+        target.fillColor = .red
+    }
 }
 
 
 func resetGame() {
     ball.position = Point(x: 0, y: -80)
+    
 }
 
 //LAYS OUT THE COMPONENTS OF THE GAME: BALL, BARRIERS (3), TARGETS(2), FUNNEL(1)
@@ -175,7 +162,7 @@ func setup() {
     addTarget(at: Point(x: 177, y: 598))
     addTarget(at: Point(x: 349, y: 522))
     addTarget(at: Point(x: 137, y: 393))
-     
+    addTarget(at: Point(x: 250, y: 325))
 }
 
 
